8ed31fb821aa2c36bbb03273d975ee2784a46842
Andy Pickering <anpicker@redhat.com>
2024-02-25 03:36:40 +0900

    Enable "Ask OpenShift Lightspeed" action on 4 more resource types
    Enable for CronJob, DaemonSet, ReplicaSet and StatefulSet These all use
    the standard details page component in the web console, so we can reuse
    the existing `K8sResourceActionsProvider` code.

--- src/components/GeneralPage.tsx
+++ src/components/GeneralPage.tsx
@@ -254,7 +254,7 @@ const GeneralPage = () => {
                   onClick={onInsertYAML}
                   variant="secondary"
                 >
-                  Insert {context.kind.toLowerCase()} YAML at cursor
+                  Insert {context.kind} YAML at cursor
                 </Button>
               </Alert>
             </>
