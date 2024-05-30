307c512f13e5b19d787f4ce9c85e8ee5125b9844
Andy Pickering <anpicker@redhat.com>
2024-05-30 14:17:50 +0900

    Sort values in the container dropdown in the attach log modal

--- src/components/GeneralPage.tsx
+++ src/components/GeneralPage.tsx
@@ -384,7 +384,7 @@ const AttachMenu: React.FC<AttachMenuProps> = ({ context }) => {
       )}
 
       <AttachLogModal
-        containers={lodashMap(context?.spec?.containers, 'name')}
+        containers={lodashMap(context?.spec?.containers, 'name')?.sort()}
         isOpen={isLogModalOpen}
         kind={kind}
         namespace={namespace}
