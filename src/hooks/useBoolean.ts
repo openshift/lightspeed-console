ea8ed044a68399289b75e38fd5e5e0dd82eb76b8
Andy Pickering <anpicker@redhat.com>
2024-07-17 21:10:29 +0900

    Fix closing containers dropdown when you click off it

--- src/hooks/useBoolean.ts
+++ src/hooks/useBoolean.ts
@@ -7,6 +7,6 @@ export const useBoolean = (
   const toggle = React.useCallback(() => setValue((v) => !v), []);
   const setTrue = React.useCallback(() => setValue(true), []);
   const setFalse = React.useCallback(() => setValue(false), []);
-  const set = React.useCallback((v) => setValue(v), []);
+  const set = React.useCallback((v: boolean) => setValue(v), []);
   return [value, toggle, setTrue, setFalse, set];
 };
