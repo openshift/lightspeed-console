b1957eb7f643c7528baa06acf310d17ca8d8869d
Haoyu Sun <hasun@redhat.com>
2025-03-25 11:34:05 +0100

    Bump to version 0.3.3

--- src/components/OverviewDetail.tsx
+++ src/components/OverviewDetail.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 
 // TODO: Dynamically get the current plugin version instead of hard-coding it here
-const OverviewDetail: React.FC = () => <>0.3.2</>;
+const OverviewDetail: React.FC = () => <>0.3.3</>;
 
 export default OverviewDetail;
