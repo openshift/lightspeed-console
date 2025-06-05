aa25472ee46926daec20d3dbc48dd74ee3691539
Andy Pickering <anpicker@redhat.com>
2025-06-05 10:28:53 +0900

    Pass unique ID to launchModal
    This ID keeps our UI separate from other web console modals

--- src/hooks/usePopover.ts
+++ src/hooks/usePopover.ts
@@ -4,6 +4,8 @@ import { useModal } from '@openshift-console/dynamic-plugin-sdk';
 import { useBoolean } from './useBoolean';
 import Popover from '../components/Popover';
 
+const POPOVER_ID = 'plugin__lightspeed-console-plugin__POPOVER_ID';
+
 const usePopover = () => {
   const [isLaunched, , setLaunched] = useBoolean(false);
 
@@ -11,7 +13,10 @@ const usePopover = () => {
 
   React.useEffect(() => {
     if (!isLaunched && launchModal) {
-      launchModal?.(Popover, {});
+      // @ts-expect-error: The plugin SDK expects launchModal to take 2 arguments, but depending on
+      // the web console version, it may actually be dynamically linked to a version that accepts
+      // the optional third ID argument
+      launchModal?.(Popover, {}, POPOVER_ID);
       setLaunched();
     }
   }, [isLaunched, launchModal, setLaunched]);
