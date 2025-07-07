23ec1ef227eefffe3c73e59b15eac5750785f25f
Andy Pickering <anpicker@redhat.com>
2025-07-07 11:32:26 +0900

    Refactor to use PatternFly AI Chatbot's components
    - Use PatternFly AI Chatbot's ChatbotHeader, MessageBar, Message &
      feedback components
    - Combines all of the prompt input logic into it's own component
    - Update tests

--- src/components/AttachmentModal.tsx
+++ src/components/AttachmentModal.tsx
@@ -170,44 +170,42 @@ const AttachmentModal: React.FC = () => {
         )}
       </p>
       {isEditing ? <Editor onChange={setEditorValue} /> : <Viewer />}
-      <div className="ols-plugin__attachment-modal-actions">
-        <Form>
-          {isEditing ? (
-            <ActionGroup>
-              <Button onClick={onSave} type="submit" variant="primary">
-                {t('Save')}
-              </Button>
-              <Button onClick={setNotEditing} variant="link">
-                {t('Cancel')}
-              </Button>
-            </ActionGroup>
-          ) : (
-            <Split>
-              <SplitItem isFilled>
+      <Form>
+        {isEditing ? (
+          <ActionGroup>
+            <Button onClick={onSave} type="submit" variant="primary">
+              {t('Save')}
+            </Button>
+            <Button onClick={setNotEditing} variant="link">
+              {t('Cancel')}
+            </Button>
+          </ActionGroup>
+        ) : (
+          <Split>
+            <SplitItem isFilled>
+              <ActionGroup>
+                {attachment?.isEditable && (
+                  <Button onClick={setEditing} type="submit" variant="primary">
+                    {t('Edit')}
+                  </Button>
+                )}
+                <Button onClick={onClose} variant="link">
+                  {t('Dismiss')}
+                </Button>
+              </ActionGroup>
+            </SplitItem>
+            {attachment?.originalValue && attachment.originalValue !== attachment.value && (
+              <SplitItem>
                 <ActionGroup>
-                  {attachment?.isEditable && (
-                    <Button onClick={setEditing} type="submit" variant="primary">
-                      {t('Edit')}
-                    </Button>
-                  )}
-                  <Button onClick={onClose} variant="link">
-                    {t('Dismiss')}
+                  <Button icon={<UndoIcon />} isDanger onClick={onRevert} variant="link">
+                    {t('Revert to original')}
                   </Button>
                 </ActionGroup>
               </SplitItem>
-              {attachment?.originalValue && attachment.originalValue !== attachment.value && (
-                <SplitItem>
-                  <ActionGroup>
-                    <Button icon={<UndoIcon />} isDanger onClick={onRevert} variant="link">
-                      {t('Revert to original')}
-                    </Button>
-                  </ActionGroup>
-                </SplitItem>
-              )}
-            </Split>
-          )}
-        </Form>
-      </div>
+            )}
+          </Split>
+        )}
+      </Form>
     </Modal>
   );
 };
