51443d1f6816b80bb54bb4bf6a4fc9dca58b408d
onmete <ondrejmetelka@outlook.com>
2025-07-16 09:20:30 +0200

    Bump to 1.0.2

--- src/components/OverviewDetail.tsx
+++ src/components/OverviewDetail.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 
 // TODO: Dynamically get the current plugin version instead of hard-coding it here
-const OverviewDetail: React.FC = () => <>1.0.1</>;
+const OverviewDetail: React.FC = () => <>1.0.2</>;
 
 export default OverviewDetail;
