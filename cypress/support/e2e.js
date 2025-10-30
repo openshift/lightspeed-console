72c67cbc468cd09eccd6f12d0c9ee87e89ec5212
Andy Pickering <anpicker@redhat.com>
2025-10-30 17:18:55 +0900

    Tests: Fix to never show the popup tour when the tests first start
    Also sets testIsolation to true so that tests run locally behavior more
    like those in CI.

--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -1,4 +1,17 @@
+/* global Cypress */
 import './commands';
 import registerCypressGrep from '@cypress/grep';
 
 registerCypressGrep();
+
+Cypress.on('window:before:load', (win) => {
+  // Prevent the guided tour popup from appearing in all tests
+  const settings = {
+    'console.guidedTour': {
+      admin: {
+        completed: true,
+      },
+    },
+  };
+  win.localStorage.setItem('console-user-settings', JSON.stringify(settings));
+});
