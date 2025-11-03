ebe934b5fb17cd50dd338453974372b67e9331d1
xavier <xavier@redhat.com>
2025-11-03 11:48:45 -0500

    updated release version to 1.0.7
    Signed-off-by: xavier <xavier@redhat.com>

--- src/components/OverviewDetail.tsx
+++ src/components/OverviewDetail.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 
 // TODO: Dynamically get the current plugin version instead of hard-coding it here
-const OverviewDetail: React.FC = () => <>1.0.6</>;
+const OverviewDetail: React.FC = () => <>1.0.7</>;
 
 export default OverviewDetail;
