5d1e45f4f3276ce089dabd30acbee752450359a9
Andy Pickering <anpicker@redhat.com>
2025-11-13 12:33:06 +0900

    Fix to allow attaching multiple alerts with the same alertname

--- src/components/AttachMenu.tsx
+++ src/components/AttachMenu.tsx
@@ -184,10 +184,27 @@ const AttachMenu: React.FC = () => {
             });
             if (alert) {
               try {
+                // Generate a unique ID for the alert
+                const sortedLabels = Object.keys(alert.labels)
+                  .sort()
+                  .map((key) => `${key}=${alert.labels[key]}`)
+                  .join(',');
+                const alertId = `${AttachmentTypes.YAML}_${kind}_${sortedLabels}`;
+
                 const yaml = dumpYAML(alert, { lineWidth: -1 }).trim();
                 dispatch(
-                  attachmentSet(AttachmentTypes.YAML, kind, name, undefined, namespace, yaml),
+                  attachmentSet(
+                    AttachmentTypes.YAML,
+                    kind,
+                    name,
+                    undefined,
+                    namespace,
+                    yaml,
+                    undefined,
+                    alertId,
+                  ),
                 );
+
                 close();
               } catch (e) {
                 setError(t('Error converting to YAML: {{e}}', { e }));
